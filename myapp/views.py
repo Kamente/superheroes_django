dce7fb59e35678ca2cdaccb1ae5c6027b56fa41f
Kamente <justinm48000@gmail.com>
2023-10-25 22:44:56 +0300

    routes

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Hero, Power, HeroPower
 
-# Create your views here.
+
+def get_powers(request):
+    powers = Power.objects.all()
+    data = [{'id': power.id, 'name': power.name,
+             'description': power.description} for power in powers]
+    return JsonResponse(data, safe=False)
+
+
+def get_power_by_id(request, id):
+    power = get_object_or_404(Power, pk=id)
+    data = {'id': power.id, 'name': power.name,
+            'description': power.description}
+    return JsonResponse(data)
